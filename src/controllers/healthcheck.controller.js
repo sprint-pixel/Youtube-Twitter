ad4e646115024af8a4d78159bb5da25d338dc7d3
sprint-pixel <sulovegaire321@gmail.com>
2025-12-27 20:34:09 +0545

    Add healthcheck controller

--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -4,7 +4,15 @@ import {asyncHandler} from "../utils/asyncHandler.js"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
-    //TODO: build a healthcheck response that simply returns the OK status as json with a message
+    const dbStatus = mongoose.connection.readyState;
+
+    if(dbStatus !== 1){ //1 means connected
+        throw new ApiError(500, "Database connection not healthy")
+    }
+
+    return res
+    .status(200)
+    .json(new ApiResponse(200, {status : "OK", dbStatus}, "Health check passed: System is up and running"))
 })
 
 export {
